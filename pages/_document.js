87699ff52bfd6ed1f5b37193c8d153a143b095c0
Mustak24 <mustak.coc786@gmail.com>
2024-10-04 06:07:24 +0530

    netlify

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <link rel="icon" type="image/x-icon" href="/favicon.webp" />
+      <script src="https://cdn.tailwindcss.com"></script>
       <body className="antialiased">
         <Main />
         <NextScript />

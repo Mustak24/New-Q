4b6893e5153dc4c05377b10b100e8996c277e7a3
Mustak24 <162583970+Mustak24@users.noreply.github.com>
2024-11-12 21:40:59 +0530

    test

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -6,112 +6,11 @@ import verifyAdminToken from "@/Function/verifyAdminToken";
 import Head from "next/head";
 
 export default function (props) {
-<<<<<<< HEAD
-  const router = useRouter();
-  const { alerts, setAlert } = props;
-  const [isLoading, setLoading] = useState(false);
-  const isAdmin =
-    router.query.username == process.env.NEXT_PUBLIC_ADMIN_USERNAME;
-
-  async function Login(e) {
-    e.preventDefault();
-    setLoading(true);
-    if (!window.navigator.onLine) {
-      setLoading(false);
-      return setAlert([
-        ...alerts,
-        {
-          type: "error",
-          title: "No Internet",
-          dec: "Login fail due to no Internet connnection.",
-        },
-      ]);
-    }
-    let formData = Object.fromEntries(new FormData(e.target));
-    setAlert([...alerts, { type: "info", title: "Wait Sending..." }]);
-=======
     const router = useRouter()
     const { alerts, setAlert } = props;
     const [isLoading, setLoading] = useState(false)
     const isAdmin = router.query.username != process.env.NEXT_PUBLIC_ADMIN_USERNAME
->>>>>>> cc889cb3c9e53463723883c8c33763bb57b3063e
 
-    let time = 3000;
-    let timer = setInterval(() => (time ? (time -= 100) : ""), 100);
-
-<<<<<<< HEAD
-    let res = await fetch(`${window.location.origin}/api/login`, {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: { "content-type": "application/json" },
-    });
-    res = await res.json();
-    setLoading(false);
-    clearInterval(timer);
-    setTimeout(() => {
-      if (res?.alert) setAlert((alerts) => [...alerts, res.alert]);
-      if (res.login) {
-        router.push("/admin/dashbord");
-        sessionStorage.setItem("token", res.token);
-      }
-    }, time);
-  }
-
-  useEffect(() => {
-    verifyAdminToken().then((res) => res && router.push("/admin/dashbord"));
-  }, []);
-
-  return (
-    <>
-      <Head>
-        <title>Admin Login</title>
-      </Head>
-      {!isAdmin ? (
-        <div className="w-screen h-screen center text-2xl font-serif">
-            !! You Are Not Admin !!
-        </div>
-      ) : (
-        <div className="w-screen h-screen center px-10">
-          <form
-            onSubmit={Login}
-            className="flex-col load-onetime-self center w-full max-w-[1000px] min-h-fit box-border gap-2"
-          >
-            <h1 className="font-serif text-2xl font-semibold">
-              Login for Dashbord
-            </h1>
-            <Input
-              name="email"
-              placeholder="Enter your Email"
-              type="email"
-              required={true}
-            />
-            <Input
-              name="password"
-              placeholder="Enter your Password"
-              type="password"
-              required={true}
-              minLength={8}
-            />
-            <div className="w-full">
-              <Button
-                innerHTML="Send"
-                tailwindcss="w-full max-sm:hidden"
-                isLoading={isLoading}
-              />
-              <Button
-                innerHTML="Send"
-                tailwindcss="w-full sm:hidden"
-                effect="active"
-                isLoading={isLoading}
-              />
-            </div>
-          </form>
-        </div>
-      )}
-    </>
-  );
-}
-=======
     async function Login(e) {
         e.preventDefault();
         setLoading(true)
@@ -166,4 +65,3 @@ export default function (props) {
         </div>)}
     </>)
 }
->>>>>>> cc889cb3c9e53463723883c8c33763bb57b3063e
